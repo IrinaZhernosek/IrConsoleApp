5838ca90171eefc537de2b7741b03807d26fb254
iryna.zharnasek <iryna.zharnasek@NBA-143-22>
2022-10-20 10:18:03 +0300

    Add project files.

---
+++ IrConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IrynaZharnasek_SaysHello
+{
+    class Program { 
+        static void Main() {
+            Console.WriteLine("Hi! What is your name?");
+
+            string st1;
+            
+            st1 = (Console.ReadLine());
+            Console.WriteLine("Nice to meet you " + st1);
+        } 
+    }
+}
+
+
+
+
+

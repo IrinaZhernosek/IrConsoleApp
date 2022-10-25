7b09cd17d1ee6651745459379dd178346e9b1046
Iryna Zharnasek <iryna.zharnasek@itechart-group.com>
2022-10-25 23:48:19 +0300

    Added Homework Task 1

--- IrConsoleApp/Program.cs
+++ IrConsoleApp/Program.cs
@@ -3,16 +3,27 @@
 
 namespace IrynaZharnasek_SaysHello
 {
-    class Program { 
-        static void Main() {
+    class Program {
+        static void Main()
+        {
+            Task1();
+        }
+        /*{
             Console.WriteLine("Hi! What is your name?");
 
             string st1;
             
             st1 = (Console.ReadLine());
             Console.WriteLine("Nice to meet you " + st1);
-        } 
-    }
+
+        } */
+    private static void Task1 ()
+        {
+            var num = 20 + 5;
+            Console.WriteLine(num);
+        }
+            
+            }
 }
 
 

d53958e5c56241b544457623bbbd26531a0243e4
Iryna zharnasekgit <iryna.zharnasek@itechart-group.com>
2022-11-03 14:46:00 +0200

    new changes for last github comments

--- IrConsoleApp/Program.cs
+++ IrConsoleApp/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main()
         {
+            //LECTURE 3
             Task1();
             Task2();
             Task3();
@@ -13,22 +14,25 @@
             Task7();
             Task8();
             Task9();
+
+            //LECTURE 4
+            /*Task10();*/
         }
 
         private static void Task1()
         {
-            var num = 20 + 5;
+            var num = 20;
+            num += 5;
             Console.WriteLine(num);
         }
 
         private static void Task2()
         {
             Console.WriteLine("Enter count of days from 1000 to 9999:");
-            ushort days = (ushort)Convert.ToUInt32(Console.ReadLine());
+            ushort days = Convert.ToUInt16(Console.ReadLine());
             byte years = (byte)(days / 365);
             byte month = (byte)((days - years * 365) / 30);
             byte day = (byte)(days - years * 365 - month * 30);
-
             Console.WriteLine($"{days} days are {years} years {month} months {day} days");
 
         }
@@ -36,8 +40,8 @@
         private static void Task3()
         {
             Console.WriteLine("Enter any number:");
-            var numb = (int)Convert.ToUInt32(Console.ReadLine());
-            var res = numb + 5;
+            var numb = Convert.ToUInt32(Console.ReadLine());
+            var res = numb + numb*2;
             Console.WriteLine($"The result is {res}");
 
         }
@@ -56,7 +60,7 @@
             Console.WriteLine($"Float type: {p5}");
             ushort p6 = 40000;
             Console.WriteLine($"Ushort type: {p6}");
-            Boolean p7 = true;
+            bool p7 = true;
             Console.WriteLine($"Boolean type: {p7}");
             byte p8 = 0;
             Console.WriteLine($"Byte type: {p8}");
@@ -66,12 +70,12 @@
         {
             //Получите от пользователя данные и запишите их в переменные с типами данных: short
             Console.WriteLine("Enter number from -32 768 to 32 767:");
-            short t1 = (short)Convert.ToInt32(Console.ReadLine());
+            short t1 = Convert.ToInt16(Console.ReadLine());
             Console.WriteLine($"Thank you! You entered {t1}");
 
             //Получите от пользователя данные и запишите их в переменные с типами данных: ulong
             Console.WriteLine("Enter number above 0:");
-            ulong t2 = (ulong)Convert.ToInt64(Console.ReadLine());
+            ulong t2 = Convert.ToUInt64(Console.ReadLine());
             Console.WriteLine($"Thank you! You entered {t2}");
 
             //Получите от пользователя данные и запишите их в переменные с типами данных:char
@@ -81,49 +85,55 @@
 
             //Получите от пользователя данные и запишите их в переменные с типами данных:double
             Console.WriteLine("Enter fractional number:");
-            double t4 = (double)Convert.ToDouble(Console.ReadLine());
+            double t4 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine($"Thank you! You entered {t4}");
         }
 
 
         private static void Task6()
         {
-            sbyte n = -5 * 7;
-            sbyte n1 = --n;
-            Console.WriteLine(n1);
+            sbyte n = -5;
+            sbyte n1 = (sbyte)(n * 7);
+            sbyte n2 = --n1;
+            Console.WriteLine(n2);
         }
 
         private static void Task7()
         {
             Console.WriteLine("Enter any number:");
-            short ch1 = (short)Convert.ToInt32(Console.ReadLine());
+            short ch1 = Convert.ToInt16(Console.ReadLine());
             if (ch1 % 2 == 0)
-            { Console.WriteLine($"The number {ch1} is even number"); }
+            {
+                Console.WriteLine($"The number {ch1} is even number");
+            }
             else
-            { Console.WriteLine($"The number {ch1} is odd number"); }
+            {
+                Console.WriteLine($"The number {ch1} is odd number");
+            }
         }
 
         private static void Task8()
         {
             Console.WriteLine("Enter any number:");
-            short a = (short)Convert.ToInt32(Console.ReadLine());
-            if (a < 50 & a != 37 & a >= 32)
-            { Console.WriteLine("Working"); }
-            else if
-                (a == 0 | a == 15)
-            { Console.WriteLine("Working"); }
+            short a = Convert.ToInt16(Console.ReadLine());
+            if (a < 50 & a != 37 & a >= 32 | a==0 | a == 15)
+            {
+                Console.WriteLine("Working");
+            }
             else
-            { Console.WriteLine("The enreded number is not matched the condition"); }
+            {
+                Console.WriteLine("The entered number is not matched the condition");
+            }
         }
 
         private static void Task9()
         {
             Console.WriteLine("Enter b1:");
-            short b1 = (short)Convert.ToInt32(Console.ReadLine());
+            short b1 = Convert.ToInt16(Console.ReadLine());
             Console.WriteLine("Enter b2:");
-            short b2 = (short)Convert.ToInt32(Console.ReadLine());
+            short b2 = Convert.ToInt16(Console.ReadLine());
             Console.WriteLine("Enter any action (* + - /):");
-            string o = (Console.ReadLine());
+            string o = Console.ReadLine();
 
             switch (o)
             {
@@ -139,11 +149,15 @@
                 case "/":
                     Console.WriteLine(b1 / b2);
                     break;
-
             }
-
         }
+        //LECTURE 4!!!!!
 
+        /*private static void Task10()
+        {
+            Console.WriteLine("Enter number above 0:");
+
+        }*/
     }
 }
 

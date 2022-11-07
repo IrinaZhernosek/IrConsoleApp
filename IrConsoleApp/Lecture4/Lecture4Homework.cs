26bfa93bca4c910320a48cf3717a6f6f1f46129e
Iryna zharnasekgit <iryna.zharnasek@itechart-group.com>
2022-11-07 23:06:05 +0200

    new class was added for lecture4

---
+++ IrConsoleApp/Lecture4/Lecture4Homework.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IrynaZharnasek_SaysHello.Lecture4
+{
+    internal class Lecture4Homework
+    {
+    }
+}

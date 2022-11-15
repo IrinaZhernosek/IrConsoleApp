ac835059f14adac33aa487038b3c91ab6bc4cd77
Iryna zharnasekgit <iryna.zharnasek@itechart-group.com>
2022-11-15 22:28:14 +0200

    Lecture5 one more commit

---
+++ ClassLibraryHW5/Class1.cs
@@ -0,0 +1,36 @@
+﻿namespace ClassLibraryHW5
+{
+    public class Employees
+    {
+        public int age;
+        public string name;
+        public string surname;
+        public string position;
+
+        public Employees(int age, string name, string surname, string position)
+        {
+            this.age = age;
+            this.name = name;
+            this.surname = surname;
+            this.position = position;
+        }
+
+        public Employees() { age = 24; name = "Jon"; surname = "Golt"; position = "engineer"; }
+
+        public void Print() => Console.WriteLine($"name {name} age:{age} surname:{surname}");
+
+    }
+
+
+    /*public class Person
+    {
+        public string name = "Undefined";   // имя
+        public int age;                     // возраст
+
+        public void Print()
+        {
+            Person Tom = new Person();  // создание объекта класса Person
+            Console.WriteLine($"Имя: {name}  Возраст: {age}");
+        }
+    }*/
+}
